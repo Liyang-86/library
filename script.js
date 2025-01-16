333f80254e35448a540ad456e8a13ae7447b47e6
Liyang-86 <177089640+Liyang-86@users.noreply.github.com>
2025-01-16 17:11:02 +0800

    prototype bookshelf

--- script.js
+++ script.js
@@ -6,15 +6,30 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+        return `${this.title}, ${this.author}, ${this.pages}, ${this.read ? 'Read' : 'Not Read'}`;
     };
 }
 
 function addBooktoLibrary(title, author, pages, read) {
-    const title = new Book(title, author, pages, read);
-    myLibrary.push(title);
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
+// TPre-add book
+addBooktoLibrary('1984', 'George Orwell', 328, true);
+addBooktoLibrary('Animal Farm', 'George Orwell', 140, true);
+addBooktoLibrary('Fahrenheit 451', 'Ray Bradbury', 250, true);
+
 function displayBook() {
-    
+    const bookShelf = document.querySelector('.bookshelf');
+    bookShelf.innerHTML = '';
+    for (const book of myLibrary) {
+        const div = document.createElement('div');
+        div.innerHTML = book.info();
+        bookShelf.appendChild(div);
+    }
+    // loop thru books in array
+    // display on DOM
 }
+
+displayBook();
