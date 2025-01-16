047122d59409959b3127cc7ee95a145dba2440fa
Liyang-86 <177089640+Liyang-86@users.noreply.github.com>
2025-01-16 18:57:04 +0800

    construct bookshelf

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read ? 'Read' : 'Not Read'}`;
+        return `${this.title}<br>${this.author}<br>${this.pages}<br>${this.read ? 'Read' : 'Not Read'}`;
     };
 }
 
@@ -15,7 +15,7 @@ function addBooktoLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-// TPre-add book
+// Pre-add book
 addBooktoLibrary('1984', 'George Orwell', 328, true);
 addBooktoLibrary('Animal Farm', 'George Orwell', 140, true);
 addBooktoLibrary('Fahrenheit 451', 'Ray Bradbury', 250, true);
@@ -26,10 +26,9 @@ function displayBook() {
     for (const book of myLibrary) {
         const div = document.createElement('div');
         div.innerHTML = book.info();
+        div.classList.add('book');
         bookShelf.appendChild(div);
     }
-    // loop thru books in array
-    // display on DOM
 }
 
 displayBook();
